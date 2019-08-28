4caddc10d656d22204eb2d2572a4b818fa6612b0
Cedric-Alonso <cedric.g.alonso@gmail.com>
2019-08-28 15:07:09 +0200

    ajout bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BaseAngularComponent } from './base-angular/base-angular.component';
+
+const routes: Routes = [
+{path: '', component: BaseAngularComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseAngularComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/base-angular/base-angular.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-base-angular',
+  templateUrl: './base-angular.component.html',
+  styleUrls: ['./base-angular.component.scss']
+})
+export class BaseAngularComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
